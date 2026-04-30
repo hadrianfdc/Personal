ce9f54a9bba2178c95018f53bb850732179c8a62
hadrianfdc <hadrian.fdc@gmail.com>
2026-04-30 15:46:14 +0800

    project enhancement

--- assets/js/main.js
+++ assets/js/main.js
@@ -220,19 +220,29 @@
   });
 
   /**
-   * Initiate portfolio lightbox 
+   * Initiate portfolio lightbox
    */
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
 
   /**
-   * Initiate portfolio details lightbox 
+   * Initiate portfolio details lightbox
    */
   const portfolioDetailsLightbox = GLightbox({
     selector: '.portfolio-details-lightbox',
     width: '90%',
     height: '90vh'
+  });
+
+  /**
+   * Project gallery lightbox (supports data-gallery grouping)
+   */
+  GLightbox({
+    selector: '.glightbox',
+    touchNavigation: true,
+    loop: true,
+    zoomable: true,
   });
 
   /**
